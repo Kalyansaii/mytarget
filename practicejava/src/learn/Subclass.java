82d52f566123ed90b99a1f41fe2f3edcf5b8b132
Kondapi Sai Kalyan <kondapi.kalyan@wipro.com>
2019-02-12 22:52:14 +0530

    Myfirstevercommit

---
+++ practicejava/src/learn/Subclass.java
@@ -0,0 +1,25 @@
+package learn;
+
+public class Subclass {
+	// Time calculation in both regular and twentyfour hour format
+	
+ /* private int hour; private int minute; private int second;
+ 
+ public void settime (int h,int m,int s){
+	 hour = ((h>=0 && h<24 ? h : 0));
+	 minute = ((m>=0 && m<60 ? m : 0));
+	  second = ((m>=0 && s<60 ? s : 0));
+	 
+	  
+ }
+public String gettime (){
+	return String.format("%2d:%2d:%2d", hour,minute,second);
+	
+}
+public String formattime(){
+	return String.format("%d:%2d:%2d %s",((hour==0 || hour==12) ? 12:hour%12),minute,second, (hour<12)? "AM" : "PM");
+	*/
+	
+	
+}
+
